38d13f3313f4fa86e4bfb311712cc56ab408f924
PrimerCambio <camilanoemitisera@gmail.com>
2022-06-05 12:56:26 -0300

    Funciona

--- assets/JS/funciones.js
+++ assets/JS/funciones.js
@@ -1,4 +1,9 @@
 const formulario = document.getElementById('agregar-gasto'); 
+var nombres = [];
+var gastos = [];
+var montos = [];
+var total = 0;
+var dividir = 0;
 
 
 //clase de Interfaz maneja todo lo relacionado al HTML
@@ -30,19 +35,42 @@ const formulario = document.getElementById('agregar-gasto');
 
               },3000);
         }
+        //Sumar los Montos
+         Calcular() {
+            total = 0;
+            for (let i = 0; i < montos.length; i++) {
+               total += parseFloat(montos[i]);              
+            }
+            dividir = parseFloat(total / montos.length);
+           
+        }
 
         //Inserta los gastos a la lista
-        agregarGastoListado(nombre, cantidad){
+        agregarGastoListado(nombre, gasto, cantidad){
+            
+            nombres.push(nombre);
+            gastos.push(gasto);
+            montos.push(cantidad);
+            this.Calcular();
+           
 
              const gastosListado = document.querySelector('#gastos ul');  
+             const nuevo = document.querySelector('#total'); 
 
              //crear un li
              const li = document.createElement('li');
              li.className = 'list-group-item d-flex justify-content-between align-items-center';
              //insertar el gasto
-             li.innerHTML = `
-                 ${nombre}
-                <span class="badge badge-primary badge-pill"> $ ${cantidad} </span>`;
+             li.innerHTML = `   
+                   
+                <span class="badge badge-primary badge-pill">  ${nombre}    </span>  
+                <span> $  ${cantidad} </span>    
+                <span class="badge badge-primary badge-pill">  ${gasto} </span>
+                `;
+                nuevo.innerHTML = `
+                <p class="alert alert-danger"> El total es $ ${total} </p>
+                <p class="alert alert-success" > Debe pagar cada uno $ ${dividir} </p>
+                `;
              //insertar el html
              gastosListado.appendChild(li);
 
@@ -55,6 +83,7 @@ formulario.addEventListener('submit',function(e){
       e.preventDefault();
 
       //leer del formulario de gastos
+      const nombrePersona = document.querySelector('#nombre').value;
       const nombreGasto = document.querySelector('#gasto').value;
       const cantidadGasto = document.querySelector('#cantidad').value;
 
@@ -62,7 +91,7 @@ formulario.addEventListener('submit',function(e){
       const ui = new Interfaz();
 
       //comprobar que los campos no esten vacios
-      if(nombreGasto === '' || cantidadGasto === ''){
+      if(nombrePersona === '' ||nombreGasto === '' || cantidadGasto === ''){
          
          //2 parametros: mensaje y tipo
          ui.imprimirMensaje('Hubo un error','error');
@@ -71,6 +100,9 @@ formulario.addEventListener('submit',function(e){
         
           //insertar en el HTML
           ui.imprimirMensaje('Correcto','correcto');
-          ui.agregarGastoListado(nombreGasto,cantidadGasto);
+          ui.agregarGastoListado(nombrePersona,nombreGasto,cantidadGasto);
       }
-});
+}
+);
+    
+    
